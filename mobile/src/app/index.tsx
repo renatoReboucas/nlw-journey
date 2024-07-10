d9e638c711b34a91a886551282e5ac2b87294a6b
renatoReboucas <renato.silva@triforsys.com.br>
2024-07-10 15:25:16 -0300

    :sparkles: feat: customizando input e default theme

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,10 +1,16 @@
-import { View,Text } from 'react-native'
+import { View, Text, Image } from "react-native"
 
 export default function index() {
   return (
-    <View className='flex flex-col items-center justify-center h-screen bg-red-500'>
-      <Text>entrou</Text>
+    <View className="flex-1 items-center justify-center">
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-8"
+        resizeMode="contain"
+      />
+      <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
+        Convide seus amigos e planeje sua{"\n"}próxima viagem!
+      </Text>
     </View>
   )
 }
- 
---
+++ mobile/src/components/input.tsx
@@ -0,0 +1,13 @@
+import { TextInput, View } from "react-native"
+
+function Input() {
+  return <View></View>
+}
+
+function Field() {
+  return <TextInput />
+}
+
+Input.Field = Field
+
+export { Input }
